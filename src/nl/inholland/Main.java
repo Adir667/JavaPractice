1c9baaeb327a293892a9c48e8ffa0cfe4aa4a972
adir7 <adir7@DESKTOP-T6S04QM>
2021-09-19 00:15:59 +0200

    22

--- src/nl/inholland/Main.java
+++ src/nl/inholland/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        Student student = new Student();
+        Student student = new Student("Adir", 27);
         student.name = "Adir";
         student.age = 27;
         student.printInformation();
