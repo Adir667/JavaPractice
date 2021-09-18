69f013fb5e63c8e9293917c38814076cd242bf98
adir7 <adir7@DESKTOP-T6S04QM>
2021-09-18 23:31:53 +0200

    Initial commit

---
+++ src/nl/inholland/Main.java
@@ -0,0 +1,12 @@
+package nl.inholland;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Student student = new Student();
+        student.name = "Adir";
+        student.age = 27;
+        student.printInformation();
+    }
+}
---
+++ src/nl/inholland/Student.java
@@ -0,0 +1,12 @@
+package nl.inholland;
+
+public class Student {
+    String name;
+    int age;
+
+    void printInformation() {
+        System.out.println("Name: "+this.name);
+        System.out.println("Age: "+this.age);
+
+    }
+}
