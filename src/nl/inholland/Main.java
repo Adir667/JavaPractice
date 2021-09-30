653206ec69a052f3f199b7eecf533c54460290e8
adir7 <adir7@DESKTOP-T6S04QM>
2021-09-30 11:13:58 +0200

    assignment1

--- src/nl/inholland/Main.java
+++ src/nl/inholland/Main.java
@@ -1,10 +1,17 @@
 package nl.inholland;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
 
-        System.out.println( "Adir");
+        System.out.println( "Enter name: ");
+        Scanner sc= new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.println( "Enter age: ");
+        int age = sc.nextInt();
+
+        System.out.println("Name: " + name + ", Age: " + age);
     }
 }
