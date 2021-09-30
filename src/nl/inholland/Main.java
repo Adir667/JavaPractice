3d81bde8d142a0e49b0042c8b715cdfe756fd390
adir7 <adir7@DESKTOP-T6S04QM>
2021-09-30 09:30:34 +0200

    Reset

--- src/nl/inholland/Main.java
+++ src/nl/inholland/Main.java
@@ -5,10 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
 
-        Teacher teacher = new Teacher("Teacher", "Teacher@inholland.nl", 2500);
-        Student student = new Student("Student", "Student@inholland.nl", "2A");
-
-        teacher.printDeails();
-        student.printDeails();
+        System.out.println( "Adir");
     }
 }
