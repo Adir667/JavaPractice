5bcd540ac2ba212a7528735483487b7e97c12e22
adir7 <adir7@DESKTOP-T6S04QM>
2021-10-07 21:45:14 +0200

    assignment3

--- src/nl/inholland/Main.java
+++ src/nl/inholland/Main.java
@@ -1,10 +1,39 @@
 package nl.inholland;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
 
-        System.out.println( "Adir");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println( "Please enter the size of your group and press [ENTER]: ");
+        int nrOfStudents = sc.nextInt();
+        System.out.println("group size: " + nrOfStudents);
+        Student [] group = new Student[nrOfStudents];
+
+        for (int i = 0; i <nrOfStudents ; i++) {
+            group[i] = new Student();
+            System.out.println("Please enter the name of student #" +(i+1)+ " and press [ENTER]:");
+            group[i].setName(sc.next());
+        }
+
+        for (int i = 0; i <nrOfStudents ; i++) {
+            System.out.println("Student #" + (i+1) + ": " + group[i].getName());
+        }
+
+        for (int i = 0; i <nrOfStudents ; i++) {
+            System.out.println("Is student #" +(i+1) + "(" + group[i].getName() + ") present? [Y/N + ENTER]:");
+            char p = sc.next().charAt(0);
+            if (p == 'y') {
+                group[i].setPresence(true);
+            }
+        }
+
+        for (int i = 0; i <nrOfStudents ; i++) {
+            System.out.println("Student #" + (i+1) + ": (" +group[i].getName() + ") | Present: " + group[i].isPresence());
+        }
     }
 }
---
+++ src/nl/inholland/Student.java
@@ -0,0 +1,23 @@
+package nl.inholland;
+
+public class Student {
+
+    private String name;
+    private boolean presence;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isPresence() {
+        return presence;
+    }
+
+    public void setPresence(boolean presence) {
+        this.presence = presence;
+    }
+}
